0432fd8d3b86069d0606335e3c4c7feaee37d1c9
aswin-dotcom <your-email@example.com>
2025-06-10 22:13:47 +0530

    First API Endpoint HTTPGET

--- webapi/Controllers/LearningAPIController.cs
+++ webapi/Controllers/LearningAPIController.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
 
 namespace webapi.Controllers
 {
+    [Route("api/LearningAPI")]
     [ApiController]
     public class LearningAPIController:ControllerBase
     {
+            [HttpGet]
+            public IEnumerable<Record> GetRecords()
+            {
+                return new List<Record>
+                {
+                    new Record { Id = 1, Name = "Record 1" },
+                    new Record { Id = 2, Name = "Record 2" },
+                    new Record { Id = 3, Name = "Record 3" }
+                };
+            }
+
+
 
     }
 }
---
+++ webapi/Models/Record.cs
@@ -0,0 +1,10 @@
+﻿namespace webapi.Models
+{
+    public class Record
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+    }
+}
