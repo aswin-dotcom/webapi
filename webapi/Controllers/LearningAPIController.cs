da21b8c8b0b693f3bd8455414ae7d8d4d10d7eb3
aswin-dotcom <your-email@example.com>
2025-06-11 21:54:55 +0530

    ResponseTypes

--- webapi/Controllers/LearningAPIController.cs
+++ webapi/Controllers/LearningAPIController.cs
@@ -10,12 +10,21 @@ namespace webapi.Controllers
     public class LearningAPIController:ControllerBase
     {
             [HttpGet]
-            public ActionResult<IEnumerable<RecordDTO>> GetRecords()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<RecordDTO>> GetRecords()
             {
                 return Ok(RecordStore.records);
             }
            [HttpGet("{id:int}")]
-           public ActionResult<RecordDTO> GetRecord(int id)
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        //[ProducesResponseType(200,Type =typeof(RecordDTO))]
+        //[ProducesResponseType(400)]
+        //[ProducesResponseType(404)]
+        public ActionResult<RecordDTO> GetRecord(int id)
            {
             if (id == 0) { 
                 return BadRequest();
